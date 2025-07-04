61a3f6bff25dc356f2af04cdd853a00f697d99ad
Marco Tulio <marcot.dns@gmail.com>
2025-07-04 14:07:48 -0300

    working o style

--- src/routes/app_routes.py
+++ src/routes/app_routes.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Blueprint, render_template, request
 from ..controllers.controllers import get_book_info, search_book
 
@@ -9,12 +8,5 @@ def return_index():
     if request.method == "POST":
         get_book_info()
 
-=======
-from flask import Blueprint, render_template
+    return render_template('index.html')
 
-blueprint = Blueprint('main', __name__)
-
-@blueprint.route('/', methods=['GET'])
-def return_index():
->>>>>>> 2d077b513ed70c4283e9295240fea592b83a36eb
-    return render_template('index.html') 
